da1489435d40071ac746e6c2a3e939bedc809844
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-02-25 13:12:55 +0530

    Added progress bar color

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,4 +1,3 @@
-import path from 'path';
 import { useEffect, useState } from 'react';
 import { CircularProgressbar,buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
@@ -27,15 +26,16 @@ export default function ProgressBar() {
     const data:Data = await responce.json()
     if(data.success){
         setValue(data.result)
+        
     }
   }
-  useEffect(()=>{
+  useEffect(()=>{//buildStyles(highPBar)
     getData()
   })
   return (
     <>
             <div className='bg-white ' style={{width:'15vw',borderRadius:"5%"}}>
-      <CircularProgressbar className='p-3' value={value[0].Consumed} minValue={0}  strokeWidth={5} styles={Math.round((value[0].Consumed/value[0].Goal)*100)>=50?buildStyles(highPBar):buildStyles(lessPBar)} maxValue={value[0].Goal}text={`${Math.round((value[0].Consumed/value[0].Goal)*100)}%`} />
+      <CircularProgressbar className='p-3' value={value[0].Consumed} minValue={0}  strokeWidth={5} styles={Math.round((value[0].Consumed/value[0].Goal)*100)>=50?(Math.round((value[0].Consumed/value[0].Goal)*100)>100?buildStyles(lessPBar):buildStyles(highPBar)):buildStyles(lessPBar)} maxValue={value[0].Goal}text={`${Math.round((value[0].Consumed/value[0].Goal)*100)}%`} />
       <h5 className='text-warning text-center py-1 table-font'>{`${value[0].Consumed}/${value[0].Goal}`}</h5>
     </div>
     </>
